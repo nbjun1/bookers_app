0ec869b6ff191c5b23e7171b372a30c5a25b9d80
nbjun1 <ec2-user@ip-172-31-24-145.ap-northeast-1.compute.internal>
2023-07-10 09:15:44 +0000

    0710 1815

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'lists/new'
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/edit'
+  get 'lists/destroy'
+  get 'books/new'
+
+  root :to => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
